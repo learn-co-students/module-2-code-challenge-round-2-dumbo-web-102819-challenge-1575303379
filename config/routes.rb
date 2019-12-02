1788a1d809196f98cdc23efc6faaf40453b3f49f
“Sadia <Sadia.islam.badhon@gmail.com>
2019-12-02 12:58:03 -0500

    done but not really

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  resources :appearances, only: [:index, :show, :new, :create, :post]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :guests, only: [:index]
-  resources :episodes, only: [:index]
+  resources :guests, only: [:index, :show, :new, :create]
+  resources :episodes, only: [:index, :show, :new, :create]
 end
